5497bc2c28a895f141264d1610e9c45546e23b6b
Nagion <skrgus2004@naver.com>
2020-07-18 18:53:56 +0900

    Mustache로 view 페이지 추가 및 게시물 CRUD 페이지 추가 + API 연동

--- src/main/java/org/example/bookProject1/service/posts/PostsService.java
+++ src/main/java/org/example/bookProject1/service/posts/PostsService.java
@@ -3,12 +3,15 @@ package org.example.bookProject1.service.posts;
 import lombok.RequiredArgsConstructor;
 import org.example.bookProject1.domain.posts.Posts;
 import org.example.bookProject1.domain.posts.PostsRepository;
+import org.example.bookProject1.web.dto.PostsListResponseDto;
 import org.example.bookProject1.web.dto.PostsResponseDto;
 import org.example.bookProject1.web.dto.PostsSaveRequestDto;
 import org.example.bookProject1.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,9 +32,25 @@ public class PostsService {
         return id;
     }
 
+    @Transactional(readOnly = true)
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
