fb8f2b6d0044fd3c377c60c9cadcfc9f4ee48504
Nagion <skrgus2004@naver.com>
2020-07-19 13:37:13 +0900

    [무중단 배포] ProfileController 추가

---
+++ src/test/java/org/example/bookProject1/web/ProfileControllerUnitTest.java
@@ -0,0 +1,41 @@
+package org.example.bookProject1.web;
+
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfileControllerUnitTest {
+
+    @Test
+    public void real_profile_would_shown(){
+        //given
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void ifno_active_profile_default_would_shown(){
+        //given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
